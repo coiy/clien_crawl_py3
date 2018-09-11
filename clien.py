bbafbe45d3c9f2b15f51702f6a8746ba8e743e4a
Sung-nyoun Kim <tityrus@gmail.com>
2018-09-11 22:12:43 +0900

    Fixed inconsistent use of tabs and spaces.

--- clien.py
+++ clien.py
@@ -11,147 +11,130 @@ import os
 BASEPATH = '/Users/coiy/dev/clien_crawl_py3'
 
 def makerss():
-	lst_title = []
-	lst_category = []
-	lst_text = []
-	lst_url = []
-	lst_pubdate = []
-	lst_author = []
-	lst_reply = []
-	lst_recom = []
+  lst_title = []
+  lst_category = []
+  lst_text = []
+  lst_url = []
+  lst_pubdate = []
+  lst_author = []
+  lst_reply = []
+  lst_recom = []
 
-	db = MySQLdb.connect(config.mysql_server, config.mysql_id, config.mysql_password, config.mysql_db, charset='utf8')
-	curs = db.cursor(MySQLdb.cursors.DictCursor)
+  db = MySQLdb.connect(config.mysql_server, config.mysql_id, config.mysql_password, config.mysql_db, charset='utf8')
+  curs = db.cursor(MySQLdb.cursors.DictCursor)
 
-	rss = PyRSS2Gen.RSS2(
-		title = "clien_crawl",
-		link = "https://www.clien.net",
-		description = "RSS_clien_hot10",
-		lastBuildDate = datetime.datetime.now(),
-		items = [] )
+  rss = PyRSS2Gen.RSS2(
+    title = "clien_crawl",
+    link = "https://www.clien.net",
+    description = "RSS_clien_hot10",
+    lastBuildDate = datetime.datetime.now(),
+    items = [] )
 
-	rowcount = curs.execute ( """SELECT * FROM rss order by pubdate DESC limit 80 """ )
+  rowcount = curs.execute( """SELECT * FROM rss order by pubdate DESC limit 80 """ )
+    
+  for r in curs.fetchall():
+    lst_title.append( r['title'] )
+    lst_category.append( r['category'] )
+    lst_text.append( r['text'] )
+    lst_url.append( r['url'] )
+    lst_pubdate.append( r['pubdate'] )
+    lst_author.append( r['author'] )
+    lst_reply.append( r['reply'] )
+    lst_recom.append( r['recom'] )
 
-	for r in curs.fetchall():
-		lst_title.append( r['title'] )
-		lst_category.append( r['category'] )
-		lst_text.append( r['text'] )
-		lst_url.append( r['url'] )		
-		lst_pubdate.append( r['pubdate'] )
-		lst_author.append( r['author'] )
-
-		lst_reply.append( r['reply'] )
-		lst_recom.append( r['recom'] )
-
+  for i, title in enumerate(lst_title):
+    #tt = "[%s][%s]" % ( str(lst_reply[i]), str(lst_recom[i]))
+    item = PyRSS2Gen.RSSItem(
+    title = lst_title[i],
+    link = 'https://www.clien.net' + lst_url[i],
+    guid = PyRSS2Gen.Guid('https://www.clien.net' + lst_url[i]),
+    description = lst_text[i],
+    # pubDate = datetime.datetime.fromtimestamp(lst_pubdate[i]),
+    pubDate = lst_pubdate[i],
+    author = lst_author[i])
+        
+    rss.items.append(item)
 	
-	for i, title in enumerate(lst_title):
-		tt = "[%s][%s]" % ( str(lst_reply[i]), str(lst_recom[i]))
-		item = PyRSS2Gen.RSSItem(
-			title = lst_title[i] + tt,
-            link = 'https://www.clien.net' + lst_url[i],
-            guid = PyRSS2Gen.Guid('https://www.clien.net' + lst_url[i]),
-            description = lst_text[i],
-            # pubDate = datetime.datetime.fromtimestamp(lst_pubdate[i]),
-			pubDate = lst_pubdate[i], 
-			author = lst_author[i] )
-			
-		rss.items.append(item)
-	
-	rss.write_xml(open(BASEPATH + 'rss_clien.htm',  'w'))
+  rss.write_xml(open("rss_clien_mac.xml",  "w"))
 
-def check_pk (url, reply, recom):
-	db = MySQLdb.connect(config.mysql_server, config.mysql_id, config.mysql_password, config.mysql_db, charset='utf8')
-	curs = db.cursor(MySQLdb.cursors.DictCursor)
+def check_pk(url, reply, recom):
+  db = MySQLdb.connect(config.mysql_server, config.mysql_id, config.mysql_password, config.mysql_db, charset='utf8')
+  curs = db.cursor(MySQLdb.cursors.DictCursor)
 		
-	bFind = False
+  bFind = False
 	
-	# 같은 주소에 덧글, 공감수 바뀌었나?
-	rowcount = curs.execute ( """SELECT reply, recom FROM rss WHERE url = %s """, url )
-	data = curs.fetchone()
-	if rowcount > 0:
-		bFind = True
-		# 값이 변경되었으면 업데이트.
-		if ( reply > data['reply']  ) or ( recom  > data['recom'] ):
-			rowcount = curs.execute ( """UPDATE rss SET reply = %s, recom = %s WHERE url = %s""", (int(reply), int(recom), url) )
-			db.commit()
+  # 같은 주소에 덧글, 공감수 바뀌었나?
+  rowcount = curs.execute ( """SELECT reply, recom FROM rss WHERE url = %s """, (url,) )
+  data = curs.fetchone()
+  if rowcount > 0:
+    bFind = True
+    # 값이 변경되었으면 업데이트.
+    if ( reply > data['reply']  ) or ( recom  > data['recom'] ):
+      rowcount = curs.execute ( """UPDATE rss SET reply = %s, recom = %s WHERE url = %s""", (int(reply), int(recom), url) )
+      db.commit()
 		
-	return bFind
+    return bFind
 	
 def insert_bbs(category, title, text, url, pubdate, author, reply, recom):
-	db = MySQLdb.connect(config.mysql_server, config.mysql_id, config.mysql_password, config.mysql_db, charset='utf8')
-	curs = db.cursor(MySQLdb.cursors.DictCursor)
-	curs.execute ( u"""INSERT INTO rss (category, title, text, url, pubdate, author, reply, recom)
-		VALUES (%s, %s, %s, %s, %s, %s, %s, %s) 
-		ON DUPLICATE KEY 
-		UPDATE reply = %s, recom = %s """, (category, title, text, url, pubdate, author, reply, recom,  reply, recom))
+  db = MySQLdb.connect(config.mysql_server, config.mysql_id, config.mysql_password, config.mysql_db, charset='utf8')
+  curs = db.cursor(MySQLdb.cursors.DictCursor)
+  curs.execute("""INSERT INTO rss (category, title, text, url, pubdate, author, reply, recom)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s) 
+    ON DUPLICATE KEY 
+    UPDATE reply = %s, recom = %s """, (category, title, text, url, pubdate, author, reply, recom,  reply, recom))
 		
-	db.commit()
+  db.commit()
 
 def pasing_url(link):
-	
-	r = requests.get(link) 
-	
-	# BeautifulSoup 로 파싱
-	soup = BeautifulSoup(r.text, "lxml")
-	elements = soup.findAll('div', {'class' : 'list_item symph_row'})
+  r = requests.get(link)
+  soup = BeautifulSoup(r.text, "lxml")
+  elements = soup.findAll("div", {"class" : "list_item symph_row"})
 
-	for el in reversed(elements):
-		title = el.a.find('span', {'data-role' : 'list-title-text'}).attrs['title']
-		url = el.find('a')['href']
-		url = url.strip()
-		print(title)
+  for el in reversed(elements):
+    title = el.a.find("span", {"data-role" : "list-title-text"}).attrs["title"]
+    url = el.find("a")["href"]
+    url = url.strip()
 		
-		try:
-			reply = el.findAll("span", {"class": "rSymph05"})
-			reply = reply[0].text
-			#print reply
-		except:
-			reply = '0'
-			#print '0'
+    try:
+      reply = el.findAll("span", {"class": "rSymph05"})
+      reply = reply[0].text
+      #print reply
+    except:
+      reply = '0'
+      #print '0'
 
-		try:
-			symph = el.find("div", {"data-role": "list-like-count"})
-			span = symph.find("span")
-			recom = span.text
-			#print recom
+    try:
+      symph = el.find("div", {"data-role": "list-like-count"})
+      span = symph.find("span")
+      recom = span.text
+      #print recom
 
-		except:
-			recom = '0'
-			#print '0'
+    except:
+      recom = '0'
+      #print '0'
 
-		category = el.find("span", {"class": "category"}).attrs["title"]
+    category = el.find("span", {"class": "category"}).attrs["title"]
 
-		
-		# 덧글 10개 이상이면 내용 가져오기
-		#if ( int(reply) >= 10 ) or ( int (recom) >= 5 ):
+    # 덧글 10개 이상이면 내용 가져오기
+    #if ( int(reply) >= 10 ) or ( int (recom) >= 5 ):
 			#기존 저장 된건가? 
 			# feedly 에 제목이 두번나온다. 일단 제목에 업데이트 금지.
-			if not check_pk (url, reply, recom):
-				r = requests.get('https://www.clien.net' + url) 
+    if not check_pk(url, reply, recom):
+      r = requests.get("https://www.clien.net" + url)
+      soup = BeautifulSoup(r.text, "lxml")
+      elements = soup.find("div", {"class": "post_article fr-view"}).text
+      text = elements
+    
 				
-				# BeautifulSoup 로 파싱
-				soup = BeautifulSoup(r.text, "lxml")
-				elements = soup.find("div", {"class": "post_article fr-view"}).text
-				text = elements
+      # 날짜
+      pdate = soup.find("div", {"class": "post_author"})
+      pubdate = pdate.find("span").text.strip()
+
+      author = soup.find("span", {"class": "contact_name"}).text.strip()
 				
-				# 날짜
-				pdate = soup.find("div", {"class": "post_author"})
-				pubdate = pdate.find("span").text.strip()
-				
-				#<button class="button-md button-report" onclick="app.articleSingo('samsung');
-				
-				# 글쓴이
-				#pauthor = soup.find("span", {"class": "contact_name"}).text.strip()
-				#auth1 = ''.join(pauthor[0].encode('utf-8'))
-				# 정규식
-				# 문자숫자\w+ 로 시작하고 끝이 따옴표이지만 포함하지 않음
-				# rex = re.search("[\w]+(?=\')", auth1)
-				author = soup.find("span", {"class": "contact_name"}).text.strip()
-				
-				insert_bbs(category, title, text, url, pubdate.strip(), author, reply, recom )
-				print ("INSERT BODY!! ")
-					
-		#continue; 
+      insert_bbs(category, title, text, url, pubdate.strip(), author, reply, recom )
+      print ("INSERT BODY!! ")
+
 
 
 		
@@ -161,30 +144,23 @@ url_list = []
 #url_list.append ('https://www.clien.net/service/board/news')
 #url_list.append ( 'https://www.clien.net/service/board/park')
 #url_list.append ( 'https://www.clien.net/service/board/lecture')
-url_list.append ( 'https://www.clien.net/service/board/cm_mac')
+url_list.append('https://www.clien.net/service/board/cm_mac')
 #url_list.append ( 'https://www.clien.net/service/board/jirum')
 
-url_list.append ( 'https://www.clien.net/service/board/cm_iphonien')
+#url_list.append ( 'https://www.clien.net/service/board/cm_iphonien')
 #url_list.append ( 'https://www.clien.net/service/board/cm_car')
-url_list.append ( 'https://www.clien.net/service/board/cm_bike')
+#url_list.append ( 'https://www.clien.net/service/board/cm_bike')
 #url_list.append ( 'https://www.clien.net/service/board/cm_havehome')
 #url_list.append ( 'https://www.clien.net/service/board/cm_nas')
 
 
 for u in url_list:
-	for i in range(3,-1,-1):
-		if i == 0: 
-			print(u) 
-			try:
-				pasing_url(u)
-			except:
-				continue
+  for i in range(3,-1,-1):
+    if i == 0:
+      print(u)
+      pasing_url(u)
+    else:
+      print(u + '&po=%d' % i)
+      pasing_url(u + '?&po=%d' % i)
 
-		else:
-			print(u + '&po=%d' % i)
-			try:
-				pasing_url(u + '?&po=%d' % i)
-			except:
-				continue
-			
 makerss()
